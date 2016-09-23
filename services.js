de2f5b45f89356d044098f86cec9d4adca9bcc74
Jake Wilson <jake.e.wilson@gmail.com>
2016-09-23 14:30:05 -0600

    Added attribute definitions for all services
    Signed-off-by: Jake Wilson <jake.e.wilson@gmail.com>

--- services.js
+++ services.js
@@ -11,37 +11,224 @@ module.exports = [{
     'type': 'input',
     'placeHolder': 'Text?'
   }, {
-    'type': 'pick'
+    'type': 'select',
+    'placeHolder': 'select a category',
+    'items': [{
+      'label': 'abstract'
+    }, {
+      'label': 'animals'
+    }, {
+      'label': 'business'
+    }, {
+      'label': 'cats'
+    }, {
+      'label': 'city'
+    }, {
+      'label': 'food'
+    }, {
+      'label': 'nightlife'
+    }, {
+      'label': 'fashion'
+    }, {
+      'label': 'people'
+    }, {
+      'label': 'nature'
+    }, {
+      'label': 'sports'
+    }, {
+      'label': 'technics'
+    }]
   }]
 }, {
   'label': 'placekitten',
-  'description': 'http://placekitten.com'
+  'description': 'http://placekitten.com',
+  'attributes': [{
+    'type': 'input',
+    'placeHolder': 'Width'
+  }, {
+    'type': 'input',
+    'placeHolder': 'Height'
+  }, {
+    'type': 'boolean',
+    'placeHolder': 'Grayscale?'
+  }]
 }, {
   'label': 'placehold.it',
-  'description': 'http://placehold.it'
+  'description': 'http://placehold.it',
+  'attributes': [{
+    'type': 'input',
+    'placeHolder': 'Width'
+  }, {
+    'type': 'input',
+    'placeHolder': 'Height'
+  }, {
+    'type': 'input',
+    'placeHolder': 'Text (optional)'
+  }, {
+    'type': 'input',
+    'placeHolder': 'Text Color (#RRGGBB)'
+  }, {
+    'type': 'input',
+    'placeHolder': 'Background Color (#RRGGBB)'
+  }]
 }, {
   'label': 'DummyImage',
-  'description': 'http://dummyimage.com'
+  'description': 'http://dummyimage.com',
+  'attributes': [{
+    'type': 'input',
+    'placeHolder': 'Width'
+  }, {
+    'type': 'input',
+    'placeHolder': 'Height'
+  }, {
+    'type': 'input',
+    'placeHolder': 'Text (optional)'
+  }, {
+    'type': 'input',
+    'placeHolder': 'Text Color (#RRGGBB)'
+  }, {
+    'type': 'input',
+    'placeHolder': 'Background Color (#RRGGBB)'
+  }, {
+    'type': 'select',
+    'placeHolder': 'Choose an image format',
+    'items': [{
+      'label': 'gif',
+      'description': '(default)'
+    }, {
+      'label': 'png'
+    }, {
+      'label': 'jpg'
+    }]
+  }]
 }, {
   'label': 'LoremFlickr',
-  'description': 'http://loremflickr.com'
+  'description': 'http://loremflickr.com',
+  'attributes': [{
+    'type': 'input',
+    'placeHolder': 'Width'
+  }, {
+    'type': 'input',
+    'placeHolder': 'Height'
+  }, {
+    'type': 'boolean',
+    'placeHolder': 'Grayscale?'
+  }, {
+    'type': 'input',
+    'placeHolder': 'Enter an optional keyword'
+  }, {
+    'type': 'select',
+    'placeHolder': 'Select a modifer',
+    'items': [{
+      'label': 'Grayscale'
+    }, {
+      'label': 'p?'
+    }, {
+      'label': 'Red'
+    }, {
+      'label': 'Green'
+    }, {
+      'label': 'Blue'
+    }]
+  }]
 }, {
   'label': 'unsplash.it',
-  'description': 'http://unsplash.it'
+  'description': 'http://unsplash.it',
+  'attributes': [{
+    'type': 'input',
+    'placeHolder': 'Width'
+  }, {
+    'type': 'input',
+    'placeHolder': 'Height'
+  }, {
+    'type': 'boolean',
+    'placeHolder': 'Grayscale?'
+  }, {
+    'type': 'boolean',
+    'placeHolder': 'Blur?'
+  }, {
+    'type': 'select',
+    'placeHolder': 'Crop Gravity',
+    'items': [{
+      'label': 'North'
+    }, {
+      'label': 'East'
+    }, {
+      'label': 'South'
+    }, {
+      'label': 'West'
+    }, {
+      'label': 'Center'
+    }]
+  }]
 }, {
   'label': 'Fake Images Please',
-  'description': 'http://fakeimg.pl'
+  'description': 'http://fakeimg.pl',
+  'attributes': [{
+    'type': 'input',
+    'placeHolder': 'Width'
+  }, {
+    'type': 'input',
+    'placeHolder': 'Height'
+  }, {
+    'type': 'input',
+    'placeHolder': 'Text (optional)'
+  }, {
+    'type': 'input',
+    'placeHolder': 'Text Color (#RRGGBB)'
+  }, {
+    'type': 'input',
+    'placeHolder': 'Text Alpha (0-255)'
+  }, {
+    'type': 'input',
+    'placeHolder': 'Background Color (#RRGGBB)'
+  }, {
+    'type': 'input',
+    'placeHolder': 'Background Alpha (0-255)'
+  }, {
+    'type': 'select',
+    'placeHolder': 'Choose a font',
+    'items': [{
+      'label': 'Lobster',
+      'description': '(default)'
+    }, {
+      'label': 'Bebas'
+    }, {
+      'label': 'Museo'
+    }]
+  }]
 }, {
   'label': 'placeskull',
-  'description': 'http://placeskull.com'
+  'description': 'http://placeskull.com',
+  'attributes': [{
+    'type': 'input',
+    'placeHolder': 'Width'
+  }, {
+    'type': 'input',
+    'placeHolder': 'Height'
+  }, {
+    'type': 'input',
+    'placeHolder': 'Skull id (0-25?)'
+  }, {
+    'type': 'input',
+    'placeHolder': 'Background Color (#RRGGBB)'
+  }, {
+    'type': 'boolean',
+    'placeHolder': 'Pring image dimensions?',
+    'items': [{
+      'label': 'Yes',
+    }, {
+      'label': 'No'
+    }]
+  }]
 }, {
   'label': 'Pipsum',
-  'description': 'http://pipsum.com'
+  'description': 'http://pipsum.com',
+  'attributes': [{
+    'type': 'input',
+    'placeHolder': 'Width'
+  }, {
+    'type': 'input',
+    'placeHolder': 'Height'
+  }]
 }];
-
-// module.exports = {
-//   'placehold.it': {
-//     'label': 'placehold.it',
-//     'url': 'http://placehold.it'
-//   }
-// };
