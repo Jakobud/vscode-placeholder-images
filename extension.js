5f42ca96389a439a8fa45f6796313ef01ee1e33b
Jake Wilson <jake.e.wilson@gmail.com>
2016-09-23 22:38:46 -0600

    Implemented reducer based Promise attribute chain
    Signed-off-by: Jake Wilson <jake.e.wilson@gmail.com>

--- extension.js
+++ extension.js
@@ -7,15 +7,42 @@ let activate = (context) => {
   let disposable = vscode.commands.registerCommand('placeholderImages.placeholderImage', () => {
     let services = require('./services');
 
-    // Services quick pick
     window.showQuickPick(services, {
-        "placeHolder": "Choose a Placeholder Image service"
-      })
-      .then((service) => {
-        console.log(service);
-      }, (err) => {
+      'placeHolder': 'Choose a Placeholder Image service'
+    }).then((service) => {
+
+      if (typeof(service) === 'undefined') {
+        console.error('No placeholder service was chosen');
+        return false;
+      }
+
+      let chain = service.attributes.reduce((previous, attribute) => {
+
+        return previous.then((previousValue) => {
+
+          return new Promise((resolve, reject) => {
+            window.showInputBox({
+              'placeHolder': attribute.placeHolder
+            }).then((value) => {
+              resolve(value);
+            }, (err) => {
+              reject(err);
+            });
+
+          });
+        });
+
+      }, Promise.resolve('start'));
+
+      chain.then((lastResult) => {
+        console.log('last result was ' + lastResult);
+      }).catch((err) => {
         console.error(err);
-      });
+      })
+
+    }, (err) => {
+      console.error(err);
+    });
   });
 
   context.subscriptions.push(disposable);
