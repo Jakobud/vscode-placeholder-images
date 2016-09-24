33edc0b98f0f23080ab0b25fe4aed5ec9d1dd412
Jake Wilson <jake.e.wilson@gmail.com>
2016-09-23 23:00:03 -0600

    Added initial attribute actions
    Signed-off-by: Jake Wilson <jake.e.wilson@gmail.com>

--- extension.js
+++ extension.js
@@ -24,23 +24,67 @@ let activate = (context) => {
       let chain = service.attributes.reduce((previous, attribute) => {
 
         return previous.then((previousValue) => {
-
           return new Promise((resolve, reject) => {
-            window.showInputBox({
-              'placeHolder': attribute.placeHolder
-            }).then((value) => {
-              resolve(value);
-            }, (err) => {
-              reject(err);
-            });
+
+            // Take appropriate action for attribute type
+            switch (attribute.type) {
+
+              case 'input':
+                // Input attribute value
+                window.showInputBox({
+                  placeHolder: attribute.placeHolder
+                }).then((value) => {
+
+                  // No input was specified
+                  if (typeof(value) === 'undefined') {
+                    reject('No input value was specified');
+                    return false;
+                  }
+
+                  console.log(typeof(value));
+                  value = parseInt(value);
+                  console.log(typeof(value));
+                  attribute.value = value;
+                  resolve();
+
+                }, (err) => {
+                  reject(err);
+                });
+
+                break;
+
+              case 'select':
+                // Pick attribute value
+                window.showQuickPick(attribute.items, {
+                  placeHolder: attribute.placeHolder
+                }).then((value) => {
+
+                  if (typeof(value) === 'undefined') {
+                    reject('No value was selected');
+                    return false;
+                  }
+
+                  attribute.value = value;
+                  resolve();
+
+                }, (err) => {
+                  reject(err);
+                })
+
+                break;
+
+              default:
+                break;
+            }
 
           });
         });
 
-      }, Promise.resolve('start'));
+      }, Promise.resolve());
 
-      chain.then((lastResult) => {
-        console.log('last result was ' + lastResult);
+      // End of the reducer chain
+      chain.then(() => {
+        console.log(service);
       }).catch((err) => {
         console.error(err);
       })
