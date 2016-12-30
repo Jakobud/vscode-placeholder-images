0f018ff935168d35c66800c65234e7ae2ac88592
Jake Wilson <jake.wilson@otterproducts.com>
2016-12-30 10:59:07 -0800

    Converted to use StandardJS
    Signed-off-by: Jake Wilson <jake.wilson@otterproducts.com>

--- extension.js
+++ extension.js
@@ -1,242 +1,210 @@
-const vscode = require('vscode');
-const window = vscode.window;
+const vscode = require('vscode')
+const window = vscode.window
 
-const Promise = require('bluebird');
-const _ = require('lodash');
+const Promise = require('bluebird')
+const _ = require('lodash')
 
 // Import services
-const servicesData = require('./services');
-let services = [];
+const servicesData = require('./services')
+let services = []
 for (let key of Object.keys(servicesData)) {
-  services.push(servicesData[key]);
+  services.push(servicesData[key])
 }
 
 let activate = (context) => {
-
   let disposable = vscode.commands.registerCommand('placeholderImages.placeholderImage', () => {
-
     // Choose from available services
     window.showQuickPick(services, {
       'placeHolder': 'Choose a Placeholder Image service'
     }).then((service) => {
-
       // No service was chosen
       if (typeof (service) === 'undefined') {
-        console.error('No placeholder service was chosen');
-        return false;
+        console.error('No placeholder service was chosen')
+        return false
       }
 
       // Clone the service object
-      service = _.cloneDeep(service);
+      service = _.cloneDeep(service)
 
       // Loop through service attributes
       let chain = service.attributes.reduce((previous, attribute, index) => {
-
         return previous.then((previousValue) => {
           return new Promise((resolve, reject) => {
-
             // Take appropriate action for attribute type
             switch (attribute.action) {
 
               // Input attribute
               case 'input':
-                return inputAttributeAction(service, index, resolve, reject);
+                return inputAttributeAction(service, index, resolve, reject)
 
               // Select attribute
               case 'select':
-                return selectAttributeAction(service, index, resolve, reject);
+                return selectAttributeAction(service, index, resolve, reject)
 
               // Select Yes/No attribute
               case 'boolean':
-                return booleanAttributeAction(service, index, resolve, reject);
+                return booleanAttributeAction(service, index, resolve, reject)
 
               default:
-                break;
+                break
             }
-
-          });
-        });
-
-      }, Promise.resolve());
+          })
+        })
+      }, Promise.resolve())
 
       // End of the reducer chain
       chain.then(() => {
-
-        console.log(service);
-
+        console.log(service)
       }).catch((err) => {
-        console.error(err);
+        console.error(err)
       })
-
     }, (err) => {
-      console.error(err);
-    });
-  });
+      console.error(err)
+    })
+  })
 
-  context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable)
 }
-exports.activate = activate;
+exports.activate = activate
 
 const inputAttributeAction = (service, index, resolve, reject) => {
-
-  const attribute = service.attributes[index];
+  const attribute = service.attributes[index]
 
   // Add optional to placeHolder
-  const placeHolder = attribute.optional ? attribute.placeHolder + ' (optional)' : attribute.placeHolder;
+  const placeHolder = attribute.optional ? attribute.placeHolder + ' (optional)' : attribute.placeHolder
 
-  let next = false;
+  let next = false
   promiseWhile(() => {
-    return !next;
+    return !next
   }, () => {
-
     return new Promise((resolve, reject) => {
-
       window.showInputBox({
         placeHolder: placeHolder
       }).then((value) => {
-
         // No input was specified
         if (typeof (value) === 'undefined') {
-          return reject('No input value specified');
+          return reject('No input value specified')
         }
 
         // Trim the specified value
-        value = value.trim();
+        value = value.trim()
 
         // Validation via regex
-        const regexType = new RegExp(attribute.regex);
+        const regexType = new RegExp(attribute.regex)
 
         // Regex test or is the value optional
         if (regexType.test(value)) {
-
           // Optional value formatting
-          if (attribute.format) {
-            value = attribute.format(value);
-          }
+          // if (attribute.format) {
+          //   value = attribute.format(value);
+          // }
 
-          //Replace url placeholder
-          service.url = service.url.replace('$' + index, value);
+          // Replace url placeholder
+          attribute.value = value
+
+          // service.url = service.url.replace('$' + index, value);
 
           // Move to the next attribute
-          next = true;
+          next = true
 
           // Accept if value is optional
         } else if (attribute.optional === true && value.length === 0) {
-
           // Replace url placeholder
-          service.url = service.url.replace('$' + index, '');
+          // service.url = service.url.replace('$' + index, '');
+          attribute.value = value
 
           // Move to the next attribute
-          next = true;
+          next = true
         }
 
-        return resolve();
-
+        return resolve()
       }, (err) => {
-        return reject(err);
-      });
-
-    });
+        return reject(err)
+      })
+    })
   }).then(() => {
-    return resolve();
+    return resolve()
   }).catch((err) => {
-    console.error(err);
-  });
-
+    console.error(err)
+  })
 }
 
 const selectAttributeAction = (service, index, resolve, reject) => {
-
-  const attribute = service.attributes[index];
+  const attribute = service.attributes[index]
 
   // Add 'None' item if attribute is optional
   if (attribute.optional) {
-    attribute.items.push('None');
+    attribute.items.push('None')
   }
 
   // Pick attribute value
   window.showQuickPick(attribute.items, {
     placeHolder: attribute.placeHolder
   }).then((value) => {
-
     if (typeof (value) === 'undefined') {
-      return reject('No value was selected');
+      return reject('No value was selected')
     }
 
-    value = value.value;
+    value = value.value
 
     // Ignore value
     if (value === 'None') {
-
       // Remove url placeholder
-      service.url = service.url.replace('$' + index, '');
-
+      service.url = service.url.replace('$' + index, '')
     } else {
-
       // Optional value formatting
       if (attribute.format) {
-        value = attribute.format(value);
+        value = attribute.format(value)
       }
 
       // Replace url placeholder
-      service.url = service.url.replace('$' + index, value);
-
+      service.url = service.url.replace('$' + index, value)
     }
 
-    return resolve();
-
+    return resolve()
   }, (err) => {
-    return reject(err);
+    return reject(err)
   })
-
 }
 
 const booleanAttributeAction = (service, index, resolve, reject) => {
-
-  const attribute = service.attributes[index];
+  const attribute = service.attributes[index]
 
   // Pick attribute value
   window.showQuickPick(['Yes', 'No'], {
     placeHolder: attribute.placeHolder
   }).then((value) => {
-
     if (typeof (value) === 'undefined') {
-      return reject('No value was selected');
+      return reject('No value was selected')
     }
 
     if (value === 'Yes') {
-
       // Optional value formatting
       if (attribute.format) {
-        value = attribute.format(value);
+        value = attribute.format(value)
       }
 
       // Replace url placeholder with value
-      service.url = service.url.replace('$' + index, value);
-
+      service.url = service.url.replace('$' + index, value)
     } else {
-
       // Remove url placeholder
-      service.url = service.url.replace('$' + index, '');
-
+      service.url = service.url.replace('$' + index, '')
     }
 
-    return resolve();
-
+    return resolve()
   }, (err) => {
-    return reject(err);
+    return reject(err)
   })
-
 }
 
 const promiseWhile = (predicate, action) => {
-  function loop() {
-    if (!predicate()) return;
-    return Promise.resolve(action()).then(loop);
+  function loop () {
+    if (!predicate()) return
+    return Promise.resolve(action()).then(loop)
   }
-  return Promise.resolve().then(loop);
+  return Promise.resolve().then(loop)
 }
 
 // this method is called when your extension is deactivated
 let deactivate = () => { }
-exports.deactivate = deactivate;
+exports.deactivate = deactivate
