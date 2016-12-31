44ef8f7b9bd1dfa1a07ef141cb0d8f1a870489c6
Jake Wilson <jake.wilson@otterproducts.com>
2016-12-31 13:00:46 -0800

    Updated unsplash.it service
    Signed-off-by: Jake Wilson <jake.wilson@otterproducts.com>

--- services/unsplash.it.js
+++ services/unsplash.it.js
@@ -1,31 +1,85 @@
 module.exports = {
   label: 'unsplash.it',
   description: 'http://unsplash.it',
-  attributes: [{
-    action: 'input',
-    placeHolder: 'Width'
-  }, {
-    action: 'input',
-    placeHolder: 'Height'
-  }, {
-    action: 'boolean',
-    placeHolder: 'Grayscale?'
-  }, {
-    action: 'boolean',
-    placeHolder: 'Blur?'
-  }, {
-    action: 'select',
-    placeHolder: 'Crop Gravity',
-    items: [{
-      label: 'North'
-    }, {
-      label: 'East'
-    }, {
-      label: 'South'
-    }, {
-      label: 'West'
-    }, {
-      label: 'Center'
-    }]
-  }]
+  attributes: {
+    width: {
+      action: 'input',
+      placeHolder: 'Width',
+      regex: '^\\d+$',
+      required: true
+    },
+    height: {
+      action: 'input',
+      placeHolder: 'Height',
+      regex: '^\\d+$'
+    },
+    random: {
+      action: 'boolean',
+      placeHolder: 'Random Image?'
+    },
+    grayscale: {
+      action: 'boolean',
+      placeHolder: 'Grayscale?'
+    },
+    blur: {
+      action: 'boolean',
+      placeHolder: 'Blur?'
+    },
+    crop: {
+      action: 'select',
+      placeHolder: 'Crop Gravity',
+      items: [{
+        label: 'Center'
+      }, {
+        label: 'North'
+      }, {
+        label: 'East'
+      }, {
+        label: 'South'
+      }, {
+        label: 'West'
+      }]
+    }
+  },
+  format: function () {
+    const attr = this.attributes
+
+    let url = 'http://unsplash.it'
+
+    // Grayscale
+    if (attr.grayscale.value === true) {
+      url += '/g'
+    }
+
+    // Width
+    url += '/' + attr.width.value
+
+    // Height
+    if (attr.height.value) {
+      url += '/' + attr.height.value
+    }
+
+    let params = []
+    // Random Image
+    if (attr.random.value === true) {
+      params.push('random')
+    }
+
+    // Blur
+    if (attr.blur.value === true) {
+      params.push('blur')
+    }
+
+    // Crop gravity direction
+    if (typeof (attr.crop.value) !== 'undefined') {
+      params.push('gravity=' + attr.crop.value)
+    }
+
+    // Append parameters
+    if (params.length > 0) {
+      url += '?' + params.join('&')
+    }
+
+    return url
+  }
 }
