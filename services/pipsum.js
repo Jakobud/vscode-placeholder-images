aa791f73ce764d68e892a7b3f19543be218bb147
Jake Wilson <jake.wilson@otterproducts.com>
2016-12-31 12:44:34 -0800

    Updated Pipsum service
    Signed-off-by: Jake Wilson <jake.wilson@otterproducts.com>

--- services/pipsum.js
+++ services/pipsum.js
@@ -1,17 +1,33 @@
 module.exports = {
   label: 'Pipsum',
   description: 'http://pipsum.com',
-  url: 'http://pipsum.com/$0$1',
-  attributes: [{
-    placeHolder: 'Width?',
-    action: 'input',
-    regex: '^\\d+$'
-  }, {
-    placeHolder: 'Height?',
-    action: 'input',
-    regex: '^\\d+$',
-    format: function (value) {
-      return 'x' + value + '.jpg'
+  attributes: {
+    'width': {
+      placeHolder: 'Width?',
+      action: 'input',
+      regex: '^\\d+$',
+      required: true
+    },
+    'height': {
+      placeHolder: 'Height?',
+      action: 'input',
+      regex: '^\\d+$',
+      required: true
     }
-  }]
+  },
+  format: function () {
+    const attr = this.attributes
+    let url = 'http://pipsum.com/'
+
+    // Width
+    url += attr.width.value
+
+    // Height
+    url += 'x' + attr.height.value
+
+    // Extension
+    url += '.jpg'
+
+    return url
+  }
 }
