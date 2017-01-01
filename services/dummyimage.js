d688d3c3994bfa1d936c65d3656174b114f29758
Jake Wilson <jake.wilson@otterproducts.com>
2016-12-31 18:44:32 -0800

    Updated dummyimage service
    Signed-off-by: Jake Wilson <jake.wilson@otterproducts.com>

--- services/dummyimage.js
+++ services/dummyimage.js
@@ -1,54 +1,77 @@
 module.exports = {
   label: 'DummyImage',
   description: 'http://dummyimage.com',
-  url: 'https://dummyimage.com/$0$1$4$3$5$2',
-  attributes: [{
-    placeHolder: 'Width?',
-    action: 'input',
-    regex: '^\\d+$'
-  }, {
-    placeHolder: 'Height?',
-    action: 'input',
-    regex: '^\\d+$',
-    format: function (value) {
-      return 'x' + value
+  attributes: {
+    width: {
+      placeHolder: 'Width?',
+      action: 'input',
+      regex: '^\\d+$',
+      required: true
+    },
+    height: {
+      placeHolder: 'Height?',
+      action: 'input',
+      regex: '^\\d+$'
+    },
+    text: {
+      placeHolder: 'Text?',
+      action: 'input',
+      regex: '^.+$'
+    },
+    textColor: {
+      placeHolder: 'Text Color (#RRGGBB or #RGB)?',
+      action: 'input',
+      regex: '^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$'
+    },
+    backgroundColor: {
+      placeHolder: 'Background Color (#RRGGBB or #RGB)?',
+      action: 'input',
+      regex: '^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$'
+    },
+    format: {
+      action: 'select',
+      placeHolder: 'Image format?',
+      items: [{
+        label: '.gif'
+      }, {
+        label: '.png'
+      }, {
+        label: '.jpg'
+      }]
     }
-  }, {
-    placeHolder: 'Text?',
-    action: 'input',
-    regex: '^.+$',
-    optional: true,
-    format: function (value) {
-      return '?text=' + encodeURIComponent(value)
+  },
+  format: function () {
+    const attr = this.attributes
+    let url = 'https://dummyimage.com/'
+
+    // Width
+    url += attr.width.value
+
+    // Height
+    if (attr.height.value) {
+      url += 'x' + attr.height.value
     }
-  }, {
-    placeHolder: 'Text Color (#RRGGBB)?',
-    action: 'input',
-    optional: true,
-    regex: '^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$',
-    format: function (value) {
-      return '/' + value.replace('#', '')
+
+    // Background Color
+    if (attr.backgroundColor.value) {
+      url += '/' + attr.backgroundColor.value.replace('#', '')
     }
-  }, {
-    placeHolder: 'Background Color (#RRGGBB)?',
-    action: 'input',
-    optional: true,
-    regex: '^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$',
-    format: function (value) {
-      return '/' + value.replace('#', '')
+
+    // Text Color
+    if (attr.textColor.value) {
+      url += '/' + attr.textColor.value.replace('#', '')
     }
-  }, {
-    action: 'select',
-    placeHolder: 'Image type?',
-    items: [{
-      label: 'gif',
-      value: '.gif'
-    }, {
-      label: 'png',
-      value: '.png'
-    }, {
-      label: 'jpg',
-      value: '.jpg'
-    }]
-  }]
+
+    // Image format
+    if (attr.format.value) {
+      url += attr.format.value
+    }
+
+    // Text
+    if (attr.text.value) {
+      url += '?text=' + encodeURIComponent(attr.text.value)
+    }
+
+    return url
+  }
 }
